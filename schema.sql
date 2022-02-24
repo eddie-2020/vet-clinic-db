60cc1b13aa71e8ccdf6012ca5a3ffab399ef6259
Edward <oluyaratosin123@gmail.com>
2022-02-24 08:55:19 +0100

    Resolve schema.sql

--- schema.sql
+++ schema.sql
@@ -3,10 +3,10 @@
 CREATE TABLE dbo.animal
 (
     id INT NOT NULL PRIMARY KEY, -- primary key column
-    name [NVARCHAR](50) NOT NULL,
-    date_of_birth [NVARCHAR](50) NOT NULL,
-    escape_attempts [NVARCHAR](50) NOT NULL,
-    neutered [NVARCHAR](50) NOT NULL,
-    weight_kg [NVARCHAR](50) NOT NULL
+    name [NVARCHAR](100) NOT NULL,
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered boolean,
+    weight_kg DECIMAL
     -- specify more columns here
 );
