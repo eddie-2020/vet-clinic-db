1e7f37c3257879a9caa76f965ebd318d9948c320
Edward <oluyaratosin123@gmail.com>
2022-02-25 10:11:53 +0100

    Query and update animal table

--- schema.sql
+++ schema.sql
@@ -3,10 +3,14 @@
 CREATE TABLE dbo.animal
 (
     id INT NOT NULL PRIMARY KEY, -- primary key column
-    name [NVARCHAR](100) NOT NULL,
+    name string NOT NULL,
     date_of_birth DATE,
     escape_attempts INT,
     neutered boolean,
     weight_kg DECIMAL
     -- specify more columns here
 );
+
+ALTER TABLE dbo.animal
+    ADD species VARCHAR(60);
+
